580bb63f4a1de3b5755c17aa9bf4f3eccd34a7af
Joel Cornett <joel.cornett@gmail.com>
2014-05-13 16:13:38 -0400

    added qedit documentation

---
+++ index-min.js
@@ -0,0 +1,1 @@
+(function(){$(function(){return $("#RecordParser").load("//raw.githubusercontent.com/jncornett/RecordParser/master/AbstractRegex/description.txt")})}).call(this);
--- index.coffee
+++ index.coffee
@@ -1,3 +1,2 @@
 $ ->
-	$ "#RecordParser"
-	.load "https://raw.githubusercontent.com/jncornett/RecordParser/master/AbstractRegex/description.txt"
+	
