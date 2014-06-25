7b9ac327711c115da356c245681d7ecc5291542b
Joel Cornett <joel.cornett@gmail.com>
2014-06-25 03:30:27 -0400

    started render script

---
+++ .tools/render_site.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# TODO: Put site rendering script in here.
+# The script will:
+#   - Convert coffeescript & less files
+#   - Minify
+#   - Automatically generate links on the main page based on folders present
+#   - etc, etc
